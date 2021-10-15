95a6af4720f7f5f402fa2397392e61952f424945
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2021-10-15 12:41:49 +0530

    Update 2032--Two-Out-of-Three-Easy.py

--- leetcode/Problems/2032--Two-Out-of-Three-Easy.py
+++ leetcode/Problems/2032--Two-Out-of-Three-Easy.py
@@ -1,14 +1,16 @@
 class Solution:
     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
-        freq = [0] * 101
-        for i in set(nums1):
-            freq[i] += 1
-        for i in set(nums2):
-            freq[i] += 1
-        for i in set(nums3):
-            freq[i] += 1
+        n1 = [0 for _ in range(100)]
+        n2 = [0 for _ in range(100)]
+        n3 = [0 for _ in range(100)]
+        for num in nums1:
+            n1[num-1] = 1
+        for num in nums2:
+            n2[num-1] = 1 
+        for num in nums3:
+            n3[num-1] = 1
         answer = []
-        for i in range(1, 101):
-            if freq[i] >= 2:
-                answer.append(i)
+        for i in range(100):
+            if (n1[i]+n2[i]+n3[i]) >= 2:
+                answer.append(i+1)
         return answer
