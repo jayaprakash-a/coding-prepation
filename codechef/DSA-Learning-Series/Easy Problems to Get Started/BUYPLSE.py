04c8595c8a15f3da390577a4b18609cfb9c48025
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2022-10-08 11:05:45 +0530

    Add comments

--- codechef/DSA-Learning-Series/Easy Problems to Get Started/BUYPLSE.py
+++ codechef/DSA-Learning-Series/Easy Problems to Get Started/BUYPLSE.py
@@ -1,6 +1,7 @@
-import math
+# Problem: https://www.codechef.com/CCSTART2/problems/BUYPLSE
 
 def main():
-  [a, b, x, y] = list(map(int, input().split(' ')))
-  print(a*x + b*y)
+    [a, b, x, y] = list(map(int, input().split(' ')))
+    print(a*x + b*y)
+
 main()
